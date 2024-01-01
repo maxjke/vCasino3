3d4485f37dbe703b1803b63a6bf56da8f2614d54
maxjke <m.jako030@gmail.com>
2024-01-01 18:58:48 +0200

    slot machine update

---
+++ vikoCasinoUnity/Assets/DataAccess/Interfaces/SlotMacine/IReelController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.DataAccess.Interfaces.SlotMacine
+{
+     interface IReelController
+    {
+        void ReelStopped();
+        void RandomizeReel(GameObject reel);
+        void ResetReels();
+    }
+}
--- vikoCasinoUnity/Assets/DataAccess/Interfaces/SlotMacine/ISpin.cs
+++ vikoCasinoUnity/Assets/DataAccess/Interfaces/SlotMacine/ISpin.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public interface ISpin 
 {
      void StartSpinning();
-     IEnumerator SpinReel();
+   
 }
